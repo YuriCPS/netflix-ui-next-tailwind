084a3dcba07d7047dfcdfa2bb74753cfdd0e0895
Yuri Carvalho <yuricps@gmail.com>
2022-05-03 01:29:53 -0300

    feat: get rating based on Brazil

--- src/components/MoviesRow/index.tsx
+++ src/components/MoviesRow/index.tsx
@@ -4,32 +4,24 @@ export type MoviesRowProps = {
   category: string
   list: any[]
   genresList: any[]
+  type: string
 };
 
-// interface Styles {
-//   bg: { [key: string]: string }
-//   common: string,
-// }
-
-// const style: Styles = {
-//   bg: {
-//     style1: '',
-//     style2: '',
-//   },
-//   common: '',
-// };
-
-const MoviesRow = ({ category, list, genresList }: MoviesRowProps) => (
+const MoviesRow = ({
+  category, list, genresList, type,
+}: MoviesRowProps) => (
   <div className="mx-20 my-8">
     <h2 className="pb-4 text-2xl font-bold">{category}</h2>
     <div className="flex h-56">
       {list.map((movie) => (
         <MovieCard
-          key={movie?.title}
+          key={movie.id}
           image={movie.backdrop_path}
           genresList={genresList}
           genres={movie.genre_ids}
           relevance={movie.vote_average}
+          id={movie.id}
+          type={type}
         />
       ))}
     </div>
